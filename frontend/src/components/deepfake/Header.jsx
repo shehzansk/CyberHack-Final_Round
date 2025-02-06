eed834be1062478574244b939bac0c850862c7ca
ShashankDahake7 <shashankdahake578@gmail.com>
2025-02-06 20:04:31 +0530

    merged

--- frontend/src/components/deepfake/Header.jsx
+++ frontend/src/components/deepfake/Header.jsx
@@ -5,7 +5,7 @@ function Header({ activeContainer }) {
         <header className="bg-dark-800 shadow-lg border-b border-dark-700 p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="flex items-center space-x-2">
-                    <span className="font-semibold text-black">Deep News</span>
+                    <span className="font-semibold text-black">Deep Fake</span>
                 </div>
             </div>
         </header>
